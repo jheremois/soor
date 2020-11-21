38f19465156e1ac04142645fe06a2ea11a80571b
jheremois <jheremy802@gmail.com>
2020-11-21 13:45:10 -0400

    header section

--- src/public/js/soor.js
+++ src/public/js/soor.js
@@ -9,3 +9,22 @@ const closeNav = ()=> {
 document.getElementById("mySidenav").style.width = "0";
 
 }
+
+const adder = document.getElementById('adder')
+
+const createTask = ()=>{
+
+    let taskAdder = document.getElementById('task_adder')
+
+    if(taskAdder.style.height !== '398px'){
+        adder.style.animation = 'alternate-reverse close linear 400ms infinite'
+        taskAdder.style.height = '398px'
+    }else{
+        adder.style.animation= 'alternate-reverse pop linear 400ms infinite'
+        adder.style.transform = 'rotate(0deg)'
+        taskAdder.style.height = '0px'
+    }
+}
+
+
+adder.addEventListener('click', ()=> createTask())
